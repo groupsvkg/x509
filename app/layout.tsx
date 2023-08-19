ddb51f33b709fe1b33ee4c43fb153e47f59e0c51
Vishal Kumar Gupta <vishal.gupta@konsentus.com>
2023-08-19 11:49:30 +0100

    Implement authentication using clerk

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import { ClerkProvider } from '@clerk/nextjs';
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'x509',
+  description:
+    'Enables parsing x509 certificate and find the certificate chain',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
+}
